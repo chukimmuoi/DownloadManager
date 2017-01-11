a44bac7586d81d7be31e5ec60491d4e73228a531
chukimmuoi <chukimmuoi@gmail.com>
2017-01-11 22:54:32 +0700

    Add method + lib

--- app/src/main/java/com/example/chukimmuoi/downloadmanager/MainActivity.java
+++ app/src/main/java/com/example/chukimmuoi/downloadmanager/MainActivity.java
@@ -1,18 +1,44 @@
 package com.example.chukimmuoi.downloadmanager;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.SparseIntArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.coolerfall.download.DownloadManager;
+import com.coolerfall.download.DownloadRequest;
+import com.coolerfall.download.Logger;
+import com.coolerfall.download.OkHttpDownloader;
+import com.coolerfall.download.Priority;
+import com.example.chukimmuoi.downloadmanager.callback.DownloadExecuteCallback;
 import com.example.chukimmuoi.downloadmanager.constanst.SystemConstanst;
 import com.example.chukimmuoi.downloadmanager.utils.LogUtils;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import pub.devrel.easypermissions.EasyPermissions;
+
 public class MainActivity extends AppCompatActivity
-                          implements SystemConstanst {
+                          implements SystemConstanst, View.OnClickListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private TextView    mTextView;
+
+    private ProgressBar mProgressBar;
+
+    private Button      mButton;
+
     private DownloadManager mDownloadManager;
+
+    private static final String[] PERMISSION = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+    private SparseIntArray ids = new SparseIntArray();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +46,69 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         LogUtils.setLog(IS_DEBUG);
+
+        initialUI();
+
+        initialDownloadManager();
+    }
+
+    private void initialUI() {
+        mTextView = (TextView) findViewById(R.id.tv_test);
+
+        mProgressBar = (ProgressBar) findViewById(R.id.prg_test);
+
+        mButton = (Button) findViewById(R.id.btn_test);
+        mButton.setOnClickListener(this);
     }
 
     private void initialDownloadManager() {
+        OkHttpClient client = new OkHttpClient.Builder().build();
+        mDownloadManager    = new DownloadManager.Builder().context(this)
+                .downloader(OkHttpDownloader.create(client))
+                .threadPoolSize(NUMBER_THREAD_POOL_SIZE_DOWNLOAD)
+                .logger(new Logger() {
+                    @Override
+                    public void log(String message) {
+                        LogUtils.d(TAG, message);
+                    }
+                }).build();
+    }
 
+    @Override
+    public void onClick(View v) {
+        if (!EasyPermissions.hasPermissions(this, PERMISSION)) {
+            EasyPermissions.requestPermissions(this, "Download need external permission", 0x01,
+                    PERMISSION);
+            return;
+        }
+
+        startDownloadManager(INDEX_DOWNLOAD_FIRST, URL_DOWNLOAD_FIRST, mProgressBar, mTextView, Priority.NORMAL);
+    }
+
+    private void startDownloadManager(int index, String url, ProgressBar progressBar,
+                                      TextView textView, Priority priority) {
+        int id = ids.get(index, -1);
+        if (mDownloadManager.isDownloading(id)) {
+            mDownloadManager.cancel(id);
+        } else {
+            DownloadRequest request = new DownloadRequest.Builder()
+                    .url(url)
+                    .downloadCallback(new DownloadExecuteCallback(progressBar, textView))
+                    .retryTime(5)
+                    .retryInterval(3, TimeUnit.SECONDS)
+                    .progressInterval(1, TimeUnit.SECONDS)
+                    .priority(priority)
+                    .allowedNetworkTypes(DownloadRequest.NETWORK_WIFI)
+                    .build();
+            int downloadId = mDownloadManager.add(request);
+            ids.put(index, downloadId);
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
+        mDownloadManager.release();
     }
 }
--- app/src/main/java/com/example/chukimmuoi/downloadmanager/constanst/SystemConstanst.java
+++ app/src/main/java/com/example/chukimmuoi/downloadmanager/constanst/SystemConstanst.java
@@ -13,4 +13,11 @@ package com.example.chukimmuoi.downloadmanager.constanst;
 public interface SystemConstanst {
 
     public static final boolean IS_DEBUG = true;
+
+    public static final int NUMBER_THREAD_POOL_SIZE_DOWNLOAD = 3;
+
+    public static final int INDEX_DOWNLOAD_FIRST = 0;
+
+    public static final String URL_DOWNLOAD_FIRST =
+            "https://dl.dropboxusercontent.com/content_link/gDqxG7QJ7RhDBmlJkiCOrkQNqw61ekfek7jodGSzOxfBbDyCAvrVHi6MN7a0rFkD/file?dl=1";
 }
